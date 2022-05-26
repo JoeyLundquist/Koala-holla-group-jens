d34bcccb41f9485ad52dd670c199a455096ca7d8
Joey Lundquist <joeylundquist91@gmail.com>
2022-05-26 13:54:12 -0500

    Have router side poo.query started, need render function to complete test

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -12,7 +12,48 @@ const pool = require('../modules/pool')
 
 
 // PUT
-koalaRouter.put()
+koalaRouter.put('/:id', (req, res) => {
+    console.log('updating transfer status of koala', req.params.id);
+    let koalaId = req.params.id;
+    
+    let transferStatus = req.body.transferStatus;
+    console.log('transfer status', transferStatus)
+
+    let changeTransferStatus;
+    if(transferStatus === 'true'){
+        changeTransferStatus = false;
+    }
+    else{
+        changeTransferStatus = true;
+    }
+    console.log('transfer status', changeTransferStatus)
+
+    const sqlQuery = `
+    UPDATE koala
+    SET "readyToTransfer" = $2
+    WHERE id = $1;
+    `
+
+    const sqlParams = [
+        koalaId,
+        changeTransferStatus
+    ];
+
+    pool.query(sqlQuery, sqlParams)
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(`Failed to PUT ${err}`);
+        res.sendStatus(500)
+    })
+
+
+
+
+
+
+})
 
 // DELETE
 
