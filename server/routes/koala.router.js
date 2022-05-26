7a61bd9e07f5fe647f319b790d45d0827b32df90
Juan Villegas <jvillegas422@gmail.com>
2022-05-26 13:27:17 -0500

    Add GET

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
@@ -5,7 +6,23 @@ const koalaRouter = express.Router();
 
 
 // GET
+router.get('/', (req, res) => {{
+    const sqlQuery = `
+        SELECT * FROM koala
+        ORDER BY "name" ASC
+    `;
 
+    pool.query(sqlQuery)
+        .then((dbRes) => {
+            console.log('DB request success', dbRes);
+            res.send(dbRes);
+        })
+        .catch((err) => {
+            console.log(`DB request failed`, err);
+
+            res.sendStatus(500);
+        });
+}});
 
 // POST
 
